eb30122378c89df145c3083a5c48d744f22de169
George Kemp <georgekemp94@gmail.com>
2024-07-18 14:42:03 -0600

    New

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
+import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: fileURLToPath(new URL("index.html", import.meta.url)),
+        iframe: fileURLToPath(new URL("iframe.html", import.meta.url)),
+      },
+    },
+  },
 });
